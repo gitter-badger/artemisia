fbe237f33dfacdd21c09c397d01230988bb65b29
Charles <chlr@groupon.com>
2016-03-18 20:55:39 -0700

    scalid version change

--- project/Commons.scala
+++ project/Commons.scala
@@ -3,7 +3,7 @@ import sbt._
 object Commons {
 
   val dependencies = Seq (
-    "org.scaldi" %% "scaldi" % "0.5.6",
+    "org.scaldi" %% "scaldi" % "0.5.7",
     "com.google.guava" % "guava" % "19.0"
   )
 
