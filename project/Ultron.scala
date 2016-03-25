8b67b4dd5e1fc0119e7b25314b99f09c16dbd3e5
Charles <chlr@groupon.com>
2016-03-25 14:11:06 -0700

    fixed akka version

--- project/Ultron.scala
+++ project/Ultron.scala
@@ -4,9 +4,9 @@ object  Ultron {
 
   val dependencies = Seq (
     "com.github.scopt" %% "scopt" % "3.3.0",
-    "com.typesafe.akka" %% "akka-actor" % "2.3.11",
+    "com.typesafe.akka" %% "akka-actor" % "2.3.14",
     "org.scalaz" %% "scalaz-core" % "7.2.0",
-    "com.typesafe.akka" % "akka-testkit_2.11" % "2.4.1" % "test"
+    "com.typesafe.akka" %% "akka-testkit" % "2.3.14" % "test"
   )
 
 }
